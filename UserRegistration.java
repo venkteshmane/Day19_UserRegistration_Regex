f4738f590a3344608a9e749df9fd02f1fc3adeb6
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-03-15 14:07:53 +0530

    UC2 Validate User Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,8 +6,6 @@ public class UserRegistrationSystem {
 //Declaring patterns
     static String firstNameRegexPattern = "^[A-Z]{1}[a-z]{3,}$";
     static String lastNameRegexPattern= "^[A-Z]{1}[a-z]{3,}$";
-    //String emailRegexPattern="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
-    String emailRegexPattern = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
 
     //  UC1- method to validate first name
     public static void validateFirstName() {
@@ -39,24 +37,9 @@ public class UserRegistrationSystem {
         }
     }
 
-    //  UC3- method to validate mail
-    public static void validateMail() {
-        System.out.print("Enter your Mail ID :- ");
-        Scanner sc = new Scanner(System.in);
-        String eMail = sc.nextLine();
-        Pattern pattern = Pattern.compile(lastNameRegexPattern);
-        Matcher matcher = pattern.matcher(eMail);
-        boolean retrurn = matcher.matches();
-        if (retrurn) {
-            System.out.println("Mail ID is Valid");
-        } else {
-            System.out.println("Mail ID is Invalid");
-        }
-    }
-
         public static void main(String args[]) {
             System.out.println("Welcome to User Registration System Program");
             validateFirstName();
             validateLastName();
-            validateMail();
     }
+ }
