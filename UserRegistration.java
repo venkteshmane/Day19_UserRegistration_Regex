c080fe496bf03349370eb41a9ac928b81de51cdb
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-03-15 15:11:37 +0530

    UC8 validate password Rule 4

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,25 +3,27 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistrationSystem {
-//Declaring patterns
-    // UC1
+//Declaring Regex patterns
+    //UC1
     public static final String firstNameRegexPattern = "^[A-Z]{1}[a-z]{3,}$";
     //UC2
-    public static final String lastNameRegexPattern= "^[A-Z]{1}[a-z]{3,}$";
+    public static final String lastNameRegexPattern = "^[A-Z]{1}[a-z]{3,}$";
     //UC3
     public static final String emailRegexPattern = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
     //String emailRegexPattern="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
     //UC4
     public static final String mobileNoFormatRegexPattern = "^[9,1]{2}?[0-9]{10}$";
     //UC5-Rule1
-    public static final String passwordRegexPatternRule1= "^[a-zA-Z0-9]{8,}$";
-          //    String regex = "^[A-Za-z]{8,}$";
+    public static final String passwordRegexPatternRule1 = "^[a-zA-Z0-9]{8,}$";
+    // String regex = "^[A-Za-z]{8,}$";
     //UC6-Rule2
     public static final String passwordRegexPatternRule2 = "^[A-Z]{1,}[a-z]{7,}$";
-       //    String passwordPatternSecondRule="^[A-Z][a-zA-Z0-9]{8,}";
-
-
-
+    // String passwordPatternSecondRule="^[A-Z][a-zA-Z0-9]{8,}";
+    //UC7-Rule3
+    public static final String passwordRegexPatternRule3 = "^[A-Z]{1,}[0-9]{1,}[a-z]{6,}$";
+    //UC8-Rule4
+    // public static final String passwordRegexPatternRule4 = "^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
+    public static final String passwordRegexPatternRule4 = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()<>]).{8,}$";
 
 
     //  UC1- method to validate first name
@@ -115,13 +117,75 @@ public class UserRegistrationSystem {
         }
     }
 
-        public static void main(String args[]) {
-            System.out.println("Welcome to User Registration System Program");
-            validateFirstName();
-            validateLastName();
-            validateMail();
-            validateMobileNumber();
-            validatePasswordRule1();
-            validatePasswordRule2();
+    // UC7- Rule3-method to validate password of minimum 1 numeric character
+    public static void validatePasswordRule3() {
+        System.out.print("Enter Password :- ");
+        Scanner sc = new Scanner(System.in);
+        String passWord = sc.next();
+        Pattern pattern = Pattern.compile(passwordRegexPatternRule3);
+        Matcher matcher = pattern.matcher(passWord);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+        }
     }
- }
+
+    // UC8- Rule4-method to validate password must contain 1 special character
+    public static void validatePasswordRule4() {
+        System.out.print("Enter Password :- ");
+        Scanner sc = new Scanner(System.in);
+        String passWord = sc.next();
+        Pattern pattern = Pattern.compile(passwordRegexPatternRule4);
+        Matcher matcher = pattern.matcher(passWord);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+        }
+    }
+
+
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to User Registration System Program");
+        System.out.println("Please select your choices");
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("1.To Validate First Name \n2.To Validate Last Name \n3.To Validate Mail Id \n4.To Validate Mobile Number \n5.To Validate Password Rule1 of Min 8 Characters " +
+                    "\6.To Validate Password Rule2 of Min One Upper case " + "\n7.To Validate Password Rule3 of One Numeric Character \n8.To Validate Password Rule4 of One Special Character");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    validateFirstName();
+                    break;
+                case 2:
+                    validateLastName();
+                    break;
+                case 3:
+                    validateMail();
+                    break;
+                case 4:
+                    validateMobileNumber();
+                    break;
+                case 5:
+                    validatePasswordRule1();
+                    break;
+                case 6:
+                    validatePasswordRule2();
+                    break;
+                case 7:
+                    validatePasswordRule3();
+                    break;
+                case 8:
+                    validatePasswordRule4();
+                    break;
+            }
+        }
+        while(choice != 8);
+    }
+}
