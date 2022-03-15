aeb2fe775cf17f59ee3367f81b788766ed3b6a1f
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-03-15 14:49:47 +0530

    UC6 validate password Rule 2

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,13 +9,17 @@ public class UserRegistrationSystem {
     //UC2
     public static final String lastNameRegexPattern= "^[A-Z]{1}[a-z]{3,}$";
     //UC3
-    //String emailRegexPattern="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
     public static final String emailRegexPattern = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
+    //String emailRegexPattern="^[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
     //UC4
     public static final String mobileNoFormatRegexPattern = "^[9,1]{2}?[0-9]{10}$";
     //UC5-Rule1
     public static final String passwordRegexPatternRule1= "^[a-zA-Z0-9]{8,}$";
-    //    String regex = "^[A-Za-z]{8,}$";
+          //    String regex = "^[A-Za-z]{8,}$";
+    //UC6-Rule2
+    public static final String passwordRegexPatternRule2 = "^[A-Z]{1,}[a-z]{7,}$";
+       //    String passwordPatternSecondRule="^[A-Z][a-zA-Z0-9]{8,}";
+
 
 
 
@@ -95,6 +99,22 @@ public class UserRegistrationSystem {
             System.out.println("Password is Invalid");
         }
     }
+
+    // UC6- Rule2-method to validate password of minimum 1 UpperCase
+    public static void validatePasswordRule2() {
+        System.out.print("Enter Password :- ");
+        Scanner sc = new Scanner(System.in);
+        String passWord = sc.next();
+        Pattern pattern = Pattern.compile(passwordRegexPatternRule2);
+        Matcher matcher = pattern.matcher(passWord);
+        boolean retrurn = matcher.matches();
+        if (retrurn) {
+            System.out.println("Password is Valid");
+        } else {
+            System.out.println("Password is Invalid");
+        }
+    }
+
         public static void main(String args[]) {
             System.out.println("Welcome to User Registration System Program");
             validateFirstName();
@@ -102,5 +122,6 @@ public class UserRegistrationSystem {
             validateMail();
             validateMobileNumber();
             validatePasswordRule1();
+            validatePasswordRule2();
     }
  }
